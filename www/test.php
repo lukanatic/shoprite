839ebfc3c8b10743e22dbd9df0f49e7b3da84762
ibrahim lukan <ibrahim8338@yahoo.com>
2017-03-28 11:27:03 +0100

    Finishing setting PDO and error handler

--- www/test.php
+++ www/test.php
@@ -7,12 +7,17 @@ define('DBPASS', 'ibrahim');
 
 
 try{
+	#prepare a PDO instance
 $pdo = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+#set verbose Error modes
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
  }
 catch(PDOException $e)
 {
 	echo $e->getMessage();
 }
+
 
 ?>
 
